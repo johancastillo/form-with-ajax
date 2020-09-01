a51906193d8f9b6a7563bc15f0baf87dd96dd071
Johan Castillo <jcjohan2707@gmail.com>
2020-09-01 10:57:57 -0400

    Insertando datos a MySQL desde PHP

--- task-add.php
+++ task-add.php
@@ -3,7 +3,17 @@
   include('database.php');
 
   if(isset($_POST['name'])){
-    echo $_POST['name'];
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $query = "INSERT INTO task (name, description) VALUES ('$name', '$description')";
+    $result = mysqli_query($connection, $query);
+
+    //Comprobar
+    if(!result){
+      die('Query Failed.');
+    }
+    echo "Task Added Successfully";
+
   }
 
  ?>
